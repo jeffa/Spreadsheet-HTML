ebbf6bbeeddb2fcac49ac35092f0f264735b9f36
jeffa <jeffa@unlocalhost.com>
2015-05-23 16:09:18 -0700

    prep for v0.09

--- lib/Spreadsheet/HTML.pm
+++ lib/Spreadsheet/HTML.pm
@@ -1,7 +1,7 @@
 package Spreadsheet::HTML;
 use strict;
 use warnings FATAL => 'all';
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use Clone;
 use HTML::Element;
--- lib/Spreadsheet/HTML/CSV.pm
+++ lib/Spreadsheet/HTML/CSV.pm
@@ -1,7 +1,7 @@
 package Spreadsheet::HTML::CSV;
 use strict;
 use warnings FATAL => 'all';
-our $VERSION = '0.00';
+our $VERSION = '0.01';
 
 use Carp;
 eval "use Text::CSV";
@@ -10,6 +10,7 @@ sub load {
     my @data;
     my $file = shift;
 
+    # TODO use Text::CSV_XS or Text::CSV or below code
     open my $fh, '<', $file or croak "Cannot read $file: $!\n";
     while (<$fh>) {
         chomp;
