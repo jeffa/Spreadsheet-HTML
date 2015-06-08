fc30923043de3aff98711c5300f2be3578fd602d
jeffa <jeffa@unlocalhost.com>
2015-06-08 15:06:33 -0400

    prep for v0.19

--- lib/Spreadsheet/HTML.pm
+++ lib/Spreadsheet/HTML.pm
@@ -1,7 +1,7 @@
 package Spreadsheet::HTML;
 use strict;
 use warnings FATAL => 'all';
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 
 use Exporter 'import';
 our @EXPORT_OK = qw( portrait generate landscape tornado flip mirror reverse earthquake tsunami );
