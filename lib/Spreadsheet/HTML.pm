ad0ae2e3c7d317e9dfc9f85cddd1a8b32e2fd8e7
jeffa <jeffa@unlocalhost.com>
2015-06-11 21:10:15 -0400

    prep for v0.22

--- lib/Spreadsheet/HTML.pm
+++ lib/Spreadsheet/HTML.pm
@@ -1,7 +1,7 @@
 package Spreadsheet::HTML;
 use strict;
 use warnings FATAL => 'all';
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 use Exporter 'import';
 our @EXPORT_OK = qw( generate portrait landscape north east south west );
