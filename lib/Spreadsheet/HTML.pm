jeffa/Spreadsheet-HTML

commit cba507b50d4479cf9925c0a6d3d62119bac24e8f

more refactoring of _process()
moved _expand_code_or_hash() back into _process()

lib/Spreadsheet/HTML.pm
@@ -136,22 +136,20 @@ sub _process {
 
         for my $col (0 .. $#{ $data->[$row] }) {
             my $tag = (!$row and !($args->{headless} or $args->{matrix})) ? 'th' : 'td';
-            my ( $cdata, $attr ) = _expand_code_or_hash( $data->[$row][$col], undef, $args->{$tag}, );
-            $args->{$tag} = [ $args->{$tag} ] unless ref( $args->{$tag} ) eq 'ARRAY';
 
-            # -cX
-            if (exists $args->{"-c$col"}) {
-                ( $cdata, $attr ) = _expand_code_or_hash( $cdata, $attr, $args->{"-c$col"} );
-            }
-
-            # -rX (overides -cX)
-            if (exists $args->{"-r$row"}) {
-                ( $cdata, $attr ) = _expand_code_or_hash( $cdata, $attr, $args->{"-r$row"} );
-            }
-
-            # -rXcX (overides -rX)
-            if (exists $args->{"-r${row}c${col}"}) {
-                ( $cdata, $attr ) = _expand_code_or_hash( $cdata, $attr, $args->{"-r${row}c${col}"} );
+            my ( $cdata, $attr ) = ( $data->[$row][$col], undef );
+            for ($tag, "-c$col", "-r$row", "-r${row}c${col}") {
+                next unless exists $args->{$_};
+                my $new_attr;
+                $args->{$_} = [ $args->{$_} ] unless ref( $args->{$_} ) eq 'ARRAY';
+                for (@{ $args->{$_} }) {
+                    if (ref($_) eq 'CODE') {
+                        $cdata = $_->($cdata);
+                    } elsif (ref($_) eq 'HASH') {
+                        $new_attr = $_;
+                    }
+                }
+                $attr = { %{ $attr || {} }, %{ $new_attr || {} } };
             }
 
             # handle empty
@@ -160,7 +158,7 @@ sub _process {
             $data->[$row][$col] = { 
                 tag => $tag, 
                 (defined( $cdata ) ? (cdata => $cdata) : ()), 
-                (keys( %$attr )  ? (attr => $attr)   : ()),
+                (keys( %$attr )    ? (attr => $attr)   : ()),
             };
         }
     }
@@ -301,21 +299,6 @@ sub _args {
     }
 
     return ( $self, Clone::clone($data), $args );
-}
-
-sub _expand_code_or_hash {
-    my ( $cdata, $attr, $thingy ) = @_;
-    my $new_attr;
-    $thingy = [ $thingy ] unless ref( $thingy ) eq 'ARRAY';
-    for (@{ $thingy }) {
-        if (ref($_) eq 'CODE') {
-            $cdata = $_->($cdata);
-        } elsif (ref($_) eq 'HASH') {
-            $new_attr = $_;
-        }
-    }
-    $attr = { %{ $attr || {} }, %{ $new_attr || {} } };
-    return ( $cdata, $attr );
 }
 
 sub _range {grep!(($_-$_[0])%($_[2]||1)),$_[0]..$_[1]}

commit 6fdee409098521dfc2a0f8fba45e1ea6782b7b31

fixed background cell animate bug
check in cba507b broke some untested functionality
partial revert of 7c5606f, will try again as cba507b
gained some speed (and am still looking for a way to
test animated cell backgrounds with javascript ...)

lib/Spreadsheet/HTML.pm
@@ -128,6 +128,8 @@ sub _process {
         }
     }
 
+    my $empty = exists $args->{empty} ? $args->{empty} : '&nbsp;';
+
     for my $row (0 .. $args->{_max_rows} - 1) {
 
         unless ($args->{_layout}) {
@@ -137,23 +139,25 @@ sub _process {
 
         for my $col (0 .. $#{ $data->[$row] }) {
             my $tag = (!$row and !($args->{headless} or $args->{matrix})) ? 'th' : 'td';
+            my ( $cdata, $attr ) = _expand_code_or_hash( $data->[$row][$col], undef, $args->{$tag}, );
+            $args->{$tag} = [ $args->{$tag} ] unless ref( $args->{$tag} ) eq 'ARRAY';
 
-            my ( $cdata, $attr ) = ( $data->[$row][$col], undef );
-            for ($tag, "-c$col", "-r$row", "-r${row}c${col}") {
-                next unless exists $args->{$_};
-                my $new_attr;
-                $args->{$_} = [ $args->{$_} ] unless ref( $args->{$_} ) eq 'ARRAY';
-                for (@{ $args->{$_} }) {
-                    if (ref($_) eq 'CODE') {
-                        $cdata = $_->($cdata);
-                    } elsif (ref($_) eq 'HASH') {
-                        $new_attr = $_;
-                    }
-                }
-                $attr = { %{ $attr || {} }, %{ $new_attr || {} } };
+            # -cX
+            if (exists $args->{"-c$col"}) {
+                ( $cdata, $attr ) = _expand_code_or_hash( $cdata, $attr, $args->{"-c$col"} );
             }
 
-            my $empty = exists $args->{empty} ? $args->{empty} : '&nbsp;';
+            # -rX (overides -cX)
+            if (exists $args->{"-r$row"}) {
+                ( $cdata, $attr ) = _expand_code_or_hash( $cdata, $attr, $args->{"-r$row"} );
+            }
+
+            # -rXcX (overides -rX)
+            if (exists $args->{"-r${row}c${col}"}) {
+                ( $cdata, $attr ) = _expand_code_or_hash( $cdata, $attr, $args->{"-r${row}c${col}"} );
+            }
+
+            # handle empty
             do{ no warnings; $cdata =~ s/^\s*$/$empty/g };
 
             $data->[$row][$col] = { 
@@ -320,6 +324,21 @@ sub _args {
     }
 
     return ( $self, Clone::clone($data), $args );
+}
+
+sub _expand_code_or_hash {
+    my ( $cdata, $attr, $thingy ) = @_;
+    my $new_attr;
+    $thingy = [ $thingy ] unless ref( $thingy ) eq 'ARRAY';
+    for (@{ $thingy }) {
+        if (ref($_) eq 'CODE') {
+            $cdata = $_->($cdata);
+        } elsif (ref($_) eq 'HASH') {
+            $new_attr = $_;
+        }
+    }
+    $attr = { %{ $attr || {} }, %{ $new_attr || {} } };
+    return ( $cdata, $attr );
 }
 
 sub _range {grep!(($_-$_[0])%($_[2]||1)),$_[0]..$_[1]}