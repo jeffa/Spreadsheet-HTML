de382dec498cb34becd281efe948aeff88a1e3c8
jeffa <jeffa@unlocalhost.com>
2015-08-25 12:54:20 -0400

    added algorithm explanation back

--- lib/Spreadsheet/HTML/Presets/TicTacToe.pm
+++ lib/Spreadsheet/HTML/Presets/TicTacToe.pm
@@ -54,6 +54,27 @@ sub _js_tmpl {
         score,
         moves,
         turn = "X",
+
+    /*
+     * To determine a win condition, each square is "tagged" from left
+     * to right, top to bottom, with successive powers of 2.  Each cell
+     * thus represents an individual bit in a 9-bit string, and a
+     * player's squares at any given time can be represented as a
+     * unique 9-bit value. A winner can thus be easily determined by
+     * checking whether the player's current 9 bits have covered any
+     * of the eight "three-in-a-row" combinations.
+     *
+     *     273                 84
+     *        \               /
+     *          1 |   2 |   4  = 7
+     *       -----+-----+-----
+     *          8 |  16 |  32  = 56
+     *       -----+-----+-----
+     *         64 | 128 | 256  = 448
+     *       =================
+     *         73   146   292
+     *
+     */
     wins = [7, 56, 448, 73, 146, 292, 273, 84],
 
     startNewGame = function () {
